2e34189f8dd0640df9cb884ebe74746a2b1b5d23
Алексей <a.l.kambulov@mail.ru>
2024-11-05 01:07:52 +0300

    Add files via upload

---
+++ task4.py
@@ -0,0 +1,8 @@
+print('4th program')
+#print(str(123.456))
+#print(int(float(123.456)*10))
+print(float(123.456)*10)
+#print(float(123.456)*10-int(float(123.456)*10))
+print(int((float(123.456)*10-int(float(123.456)*10))*10))
+
+
