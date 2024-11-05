e0029b69ae910453d524e84a721d696cd938e57d
Алексей <a.l.kambulov@mail.ru>
2024-11-05 10:01:29 +0300

    Update task4.py

--- task4.py
+++ task4.py
@@ -4,3 +4,5 @@ print('4th program')
 print(float(123.456)*10)
 print(int((float(123.456)*10-int(float(123.456)*10))//0.1))
 print(int((float(123.456)*10-int(float(123.456)*10))*10))
+print(int(((float(123.456)*10-int(float(123.456)*10)) % 1)*10))
+#print(int((1234.56 % 1)*10)) # более проще, но думаю вариантов ещё с 10 найдётся.
